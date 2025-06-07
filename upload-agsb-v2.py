0f7f1a254f87729fcffc87b07537d37bb2995e4e
shinesha0906 <64880932+shinesha0906@users.noreply.github.com>
2025-06-07 09:53:23 +0800

    Update upload-agsb-v2.py

--- upload-agsb-v2.py
+++ upload-agsb-v2.py
@@ -31,11 +31,11 @@ DEBUG_LOG = INSTALL_DIR / "python_debug.log"
 CUSTOM_DOMAIN_FILE = INSTALL_DIR / "custom_domain.txt" # 存储最终使用的域名
 
 # ====== 全局可配置参数（可直接在此处修改） ======
-USER_NAME = "kkddytd"         # 用户名
-UUID = "a91b59b6-ade4-497d-b4e9-88d184c48048"                     # UUID，留空则自动生成
-PORT = 49999                   # Vmess端口，留空或0则自动生成
-DOMAIN = "free.testlrj.ddns-ip.net"                   # 域名，留空则自动获取
-CF_TOKEN = "eyJhIjoiODBmMjY5ZmQ1N2QzNzNiMmMzZTBkODc4ODg1NWM5MzIiLCJ0IjoiZmVhMzBmODUtOGY5OC00ZTVmLTkyZTktMmU2OTk2M2E1YzUyIiwicyI6Ik4yTmlZemxpTlRjdE5UVm1PQzAwTjJZekxXRmpORGt0TVdVNE5HUmtORGN3TldObSJ9"                 # Cloudflare Token，留空则用Quick Tunnel
+USER_NAME = "yunguwen"         # 用户名
+UUID = "7e682ad2-234e-4f4b-9c4d-e64517fb290d"                     # UUID，留空则自动生成
+PORT =42001                   # Vmess端口，留空或0则自动生成
+DOMAIN = "stm01.yunguwen.online"                   # 域名，留空则自动获取
+CF_TOKEN = "eyJhIjoiNTg4ZTU3NDY2M2RkZDhhYzBhNWJmNGEzMDMxYjY4MmUiLCJ0IjoiOWRlYzVlNDMtMTA4NS00N2M3LTk1MGQtMTJlNDY2YzU3OTBmIiwicyI6Ik9UWmlPVGxrWWprdE9HUTJNUzAwTkRobExXSmtOMlV0TUdRd05tWTNOV0kyTlRabSJ9"   # Cloudflare Token，留空则用Quick Tunnel
 # =========================================
 
 # 添加命令行参数解析
